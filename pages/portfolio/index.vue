8c95fe88245c033942f4bf628b663b3d730625be
Will Aronow <waronow123@gmail.com>
2020-10-09 16:39:15 -0700

    created basic layout for portfolio page

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,13 +1,89 @@
 <template>
-  <div class="container">
-    <h1>
-      Portfolio Page
-    </h1>
-  </div>
+  <section class="container">
+    <article class="portfolio--projects-container">
+      <div class="portfolio--projects--cards">
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+        <div class="projects--card">
+          <img src="" alt="project picture placeholder">
+          <div class="projects--card-text">
+            <p>
+              project description
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article id="contact-form" class="portfolio--contact-container">
+      <div class="home--contact-text">
+        <h2>
+          CONTACT US
+        </h2>
+        <p>
+          Want a price quote or have questions regarding services?
+          <br>
+          Do you have an idea or project you want to bring to lfe?
+        </p>
+      </div>
+      <img src="" alt="home contact image placeholder">
+      <ContactForm />
+    </article>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  head () {
+    return {
+      title: 'Portfolio | Demon Dog Software',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'we can create projects using node.js for backend and frontend projects using vanilla or any javascript framework.  we also build wordpress and shiopify stores'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>
