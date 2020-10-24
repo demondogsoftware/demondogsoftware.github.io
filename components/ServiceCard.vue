3c4e7607acd200455c1242981d95d0bf466c9c15
Will Aronow <waronow123@gmail.com>
2020-10-24 11:30:33 -0700

    created service card component using props to use with v-for loop

---
+++ components/ServiceCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="service--card my-1">
+    <img :src="img" alt="service image placeholder">
+    <p>{{ service }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: String,
+    service: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
