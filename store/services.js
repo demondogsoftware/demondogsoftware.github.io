ec76c660cc87c3c16bac15abc53c5c1998f08da1
Will Aronow <waronow123@gmail.com>
2020-10-24 15:21:51 -0700

    created vuex stores for services, contact and blog

---
+++ store/services.js
@@ -0,0 +1,13 @@
+export const state = () => ({
+  services: [
+    {img: "", alt: "", description: ""},
+    {img: "", alt: "", description: ""},
+    {img: "", alt: "", description: ""}
+  ]
+})
+
+export const mutations = {}
+
+export const actions = {}
+
+export const getters = {}
